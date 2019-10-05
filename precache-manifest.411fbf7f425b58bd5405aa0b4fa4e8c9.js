a325246231cf31c17e24bfe2068dffc5b1dffc6d
shore-lab <googlapideveloper@gmail.com>
2019-10-05 22:15:58 +0600

    Updates

---
+++ precache-manifest.411fbf7f425b58bd5405aa0b4fa4e8c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7cdb6e6e1585247de643375e8d10904",
+    "url": "/countrywiki/index.html"
+  },
+  {
+    "revision": "3417329294b2a573ba5b",
+    "url": "/countrywiki/static/css/2.0501a8b0.chunk.css"
+  },
+  {
+    "revision": "c3204cdb9e07e6c4d2df",
+    "url": "/countrywiki/static/css/main.f200e1bd.chunk.css"
+  },
+  {
+    "revision": "3417329294b2a573ba5b",
+    "url": "/countrywiki/static/js/2.c86b4fa4.chunk.js"
+  },
+  {
+    "revision": "c3204cdb9e07e6c4d2df",
+    "url": "/countrywiki/static/js/main.26586a38.chunk.js"
+  },
+  {
+    "revision": "053ea19ff15c40d9d1d7",
+    "url": "/countrywiki/static/js/runtime-main.61bdab06.js"
+  },
+  {
+    "revision": "ebad1aad324e697f1d5acf68dd5eab25",
+    "url": "/countrywiki/static/media/logo.ebad1aad.png"
+  }
+]);
